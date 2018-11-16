01c3f13e3b2f2d5b95533c65d0148c4e9b2a2d31
paluchasz <paluchasz@googlemail.com>
2018-11-16 11:42:33 +0000

    A simple example of how a Java Script file can be loaded in a HTML file

---
+++ html5/basic_script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed"
+}
+// This function goes to the right part of the HTML script using the id "demo"
+// and then changes it with innerHTML() method
+// Using the document.write() method would overwrite the whole html page - should
+// only be used for testing
+// The window.alert() method would create an alert box to display data
+// Finally console.log() method can be used for debugging
